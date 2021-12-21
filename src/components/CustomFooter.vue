cb55d94691bc8921fea80804950306535016a11a
Adriano Tumino <adrianotumino@gmail.com>
2021-12-21 18:45:55 +0100

    Added Author on footer

--- src/components/CustomFooter.vue
+++ src/components/CustomFooter.vue
@@ -2,7 +2,7 @@
   <b-container class="footer_container">
     <b-row>
       <b-col md="4" sm="12" class="icon-column"> <FooterIcon /> </b-col>
-      <b-col md="8" sm="12" class="content-column"></b-col>
+      <b-col md="8" sm="12" class="content-column"> Adriano Tumino <br/>&copy; 2021</b-col>
     </b-row>
   </b-container>
 </template>
@@ -18,4 +18,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.content-column{
+  padding: 1em 0 0 0;
+  text-align: right;
+  color: whitesmoke;
+  font-size: 15px;
+}
+</style>
