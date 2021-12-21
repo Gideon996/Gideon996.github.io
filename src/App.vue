afcab0f83238c769a39773bb328b6a8b846fcd87
Adriano Tumino <adrianotumino@gmail.com>
2021-12-21 12:46:12 +0100

    Vue template

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,113 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <!-- main menu -->
+    <b-navbar toggleable="md" sticky class="navbar-expand-md">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav class="navbar-items">
+        <b-navbar-nav>
+          <b-nav-item class="item" to="home" active>HomePage</b-nav-item>
+          <b-nav-item class="item" to="cv">CV</b-nav-item>
+          <b-nav-item class="item" to="contatti">Contatti</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <!-- website content -->
+    <div class="content">
+      <p> CIAO </p>
+    </div>
+
+    <!-- footer -->
+    <footer>
+      <p>INFO</p>
+    </footer>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  name: "App",
+  components: {
+  },
+};
+</script>
+
 <style>
+:root{
+  --black: #222831;
+  --dark_grey: #393E46;
+  --grey: #EEEEEE;
+  --teal: #00ADB5;
+}
+
+html,
+body {
+  background-color: var(--grey) !important;
+  position: relative;
+  min-height: 100%;
+  min-height: 100vh;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--dark_grey);
+}
+@media (min-width: 769px) {
+  .main {
+    max-width: 80%;
+    margin: auto;
+  }
+  .navbar-items {
+    max-width: 80%;
+    margin: auto;
+  }
 }
 
-#nav {
-  padding: 30px;
+.main {
+  margin-top: 1.2em;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.navbar-expand-md {
+  background-color: #1abab5;
+  font-size: 1.2em;
+  font-weight: bolder;
+}
+.item {
+  margin-right: 1em;
+  margin-left: 1em;
+}
+.navbar-light .navbar-nav .nav-link {
+  color: #daf5f5 !important;
+}
+.navbar-light .navbar-toggler {
+  color: #daf5f5 !important;
+  border-color: #daf5f5 !important;
+}
+.navbar-light .navbar-nav .nav-link:hover {
+  color: white !important;
+  transform: scale(1.05);
+}
+.navbar-light .navbar-toggler-icon {
+  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='%23daf5f5' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e") !important;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navbar-light .navbar-nav .active{
+  color: white !important;
+  transform: scale(1.15);
 }
+
+
+.content {
+  height: auto !important;
+}
+
+footer{
+  position: absolute;
+  clear: both;
+  width: 100%;
+  bottom: 0;
+  background: var(--teal);
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
+
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+Vue.config.productionTip = false
+
+Vue.component('v-icon', Icon)
+Vue.use(BootstrapVue)
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
