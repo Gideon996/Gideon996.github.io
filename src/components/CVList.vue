c397bc96984a1dc678885b8ea758e5ab861daa62
Adriano Tumino <adrianotumino@gmail.com>
2021-12-26 17:19:23 +0100

    Added CV section

---
+++ src/components/CVList.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li class="cv-list" v-for="element in cv_list" v-bind:key="element.id">
+      <div class="element">
+        <p class="element-title">{{ element.titolo }}</p>
+        <p class="element-details"> {{ element.luogo }} - {{ element.data }} </p>
+        <p class="element-tdescription">{{ element.descrizione }}</p>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+// @ is an alias to /src
+import json from "../json/cv.json";
+
+export default {
+  data() {
+    return {
+      cv_list: json,
+    };
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.cv-list{
+    width: 80%;
+    padding: 0.5em;
+}
+.element-title{
+    font-size: 1.3em;
+    color: blue;
+    border-bottom: 2px solid black;
+}
+
+.element-details{
+    margin: auto;
+    margin-top: -0.5em;
+    font-size: 0.8em;
+}
+</style>
--- src/views/CV.vue
+++ src/views/CV.vue
@@ -1,27 +1,40 @@
 <template>
-  <div class="home">
-    <h1>SITO IN COSTRUZIONE</h1>
-    <img
-      src="https://giffiles.alphacoders.com/141/14109.gif"
-      alt="Worm Dance"
-      width="100%"
-    />
-  </div>
+  <b-container class="cv-container">
+    <b-row>
+      <b-col md="4" sm="12"> <Biography /> </b-col>
+      <b-col md="8" sm="12" class="cv-list"> <CVList /></b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
+import CVList from "../components/CVList.vue";
+import Biography from "../components/Biography.vue";
 
 export default {
   name: "cv",
-   title () {
-    return `AT - ${this.page_title}`
+  title() {
+    return `AT - ${this.page_title}`;
   },
-  data () {
+  data() {
     return {
-      page_title: 'CV'
-    }
+      page_title: "CV",
+    };
   },
-  components: {},
+  components: {
+    CVList,
+    Biography,
+  },
 };
 </script>
+
+<style scoped>
+.cv-container {
+  margin-top: 1.2em;
+}
+.cv-list {
+  text-align: left;
+  margin-bottom: 3em;
+}
+</style>
