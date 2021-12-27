d69cc338563ac4a76317b2ce6fee4e915da9d733
Adriano Tumino <adrianotumino@gmail.com>
2021-12-27 11:30:41 +0100

    Added Home Page

---
+++ src/components/SummaryBio.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="summary">
+    <h1 class="summary-title">Chi Sono</h1>
+    <div class="summary-description">
+      <p>
+        Mi chiamo Adriano Tumino e sono uno studente magistrale di Ingegneria
+        Informatica presso l'
+        <a href="https://www.units.it/" target="_blank">
+          Università degli Studi di Trieste
+        </a>
+        . Al momento sto completando i miei studi.
+      </p>
+
+      <p>Il sito nasce principalmente per poter imparare ad usare VueJS.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.summary {
+  padding: 1em;
+  width: 80%;
+  justify-content: center;
+}
+.summary-title {
+  font-size: 3em;
+  color: teal;
+}
+
+.summary-description {
+  font-size: 1.5em;
+  text-align: left;
+}
+
+a:hover,
+a:visited,
+a:link,
+a:active {
+  text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,39 @@
 <template>
-  <div class="home">
-     <h1>SITO IN COSTRUZIONE</h1>
-      <img
-        src="https://giffiles.alphacoders.com/141/14109.gif"
-        alt="Worm Dance"
-        width="100%"
-      />
-  </div>
+  <b-container class="home-container">
+    <b-row>
+      <b-col md="4" sm="12">
+        <img
+          class="home-image"
+          src="https://i.pinimg.com/originals/ab/7d/a3/ab7da3269b3b8430b4ba8430039ad4ca.gif"
+          alt="christmass tree gif"
+        />
+      </b-col>
+      <b-col md="8" sm="12" class="home-bio"> <SummaryBio /></b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import SummaryBio from "../components/SummaryBio.vue";
 export default {
-  name: 'Home',
-  title () {
-    return `AT - ${this.page_title}`
+  name: "Home",
+  title() {
+    return `AT - ${this.page_title}`;
   },
-  data () {
+  data() {
     return {
-      page_title: 'Home'
-    }
+      page_title: "Home",
+    };
   },
   components: {
-  }
-}
+    SummaryBio,
+  },
+};
 </script>
+
+<style scoped>
+.home-image{
+  padding: 1em;
+}
+</style>
